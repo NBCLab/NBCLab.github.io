99c6dec5eb66854d6cfbe693ed04ac84b21a3c29
Taylor Salo <tsalo006@fiu.edu>
2020-11-02 14:23:11 -0500

    Standardize project image sizes.
    Thanks to @angielaird for updating the pictures by hand.

--- tests/test_images.py
+++ tests/test_images.py
@@ -34,3 +34,32 @@ def test_team_image_sizes(testdata):
     if len(failures):
         failures = sorted(list(set(failures)))
         raise Exception('Test failed on {}'.format(', '.join(failures)))
+
+
+def test_project_image_sizes(testdata):
+    """
+    Test that project photos are the right size.
+    """
+    photo_folder = os.path.join(testdata["assets"], "images", "projects")
+    target_dims = (400, 400)  # in pixels
+    target_ratio = 1  # width to height
+    # Group photos should not have "-" in the filenames
+    patterns = ["*-*.png", "*-*.jpg"]
+    files = [glob(os.path.join(photo_folder, p)) for p in patterns]
+    files = [item for sublist in files for item in sublist]
+    if not len(files):
+        raise Exception("No files found in {}".format(photo_folder))
+
+    failures = []
+    for f in files:
+        fname = os.path.basename(f)
+        img = plt.imread(f)
+        if (img.shape[0] > target_dims[0]) or (img.shape[1] > target_dims[1]):
+            failures.append(fname)
+
+        if (img.shape[0] / img.shape[1]) != target_ratio:
+            failures.append(fname)
+
+    if len(failures):
+        failures = sorted(list(set(failures)))
+        raise Exception('Test failed on {}'.format(', '.join(failures)))
